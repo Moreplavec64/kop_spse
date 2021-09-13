09c914e055b0e3cc76949f6654236cfae1638f34
Moreplavec64 <adamkohadar77@gmail.com>
2021-09-13 19:44:21 +0200

    (+) response

--- lib/edupage/edu_main.dart
+++ lib/edupage/edu_main.dart
@@ -1,7 +1,7 @@
 // ignore: import_of_legacy_library_into_null_safe
 //import 'package:requests/requests.dart';
 
-//import 'dart:developer' as dev;
+import 'dart:developer' as dev;
 
 import 'package:http/http.dart' as http;
 
@@ -9,9 +9,9 @@ class EduPage {
   final String school;
   final String username;
   final String password;
-  bool isLoggedIn = false;
-
   EduPage(this.school, this.username, this.password);
+
+  bool isLoggedIn = false;
 
   void login() async {
     String requestUrl = 'https://$school.edupage.org/login/index.php';
@@ -29,8 +29,8 @@ class EduPage {
 
       print(csrfToken.toString().length);
 
-      final Uri loginRequestUrl =
-          Uri.parse("https://$school.edupage.org/login/edubarLogin.php");
+      final String loginRequestUrl =
+          'https://$school.edupage.org/login/edubarLogin.php';
 
       Map<String, String> parameters = {
         "username": username,
@@ -38,10 +38,21 @@ class EduPage {
         "csrfauth": csrfToken.toString(),
       };
 
-      final loginResponse = await http.post(loginRequestUrl, body: parameters);
+      final loginResponse =
+          await http.post(Uri.parse(requestUrl), body: parameters);
 
-      print(loginResponse.body);
-      print(loginResponse.contentLength);
+      /*
+      print(loginResponse.headers);
+      print(loginResponse.headers.entries);
+      print(loginResponse.statusCode);
+      print(loginResponse.headers['set-cookie']);
+      */
+
+      /*
+      dev.log(http
+          .get('https://spojenaskolanz.edupage.org/login/index.php')
+          .toString());
+      */
 
       /*
 
@@ -60,48 +71,4 @@ class EduPage {
       print(e.toString());
     }
   }
-
-  /* init
-  def __init__(self, school, username, password):
-        self.school = school
-        self.username = username
-        self.password = password
-        self.is_logged_in = False
-        self.session = requests.session()
-
-        # Set a timeout for all requests made with this session
-        self.session.request = functools.partial(self.session.request, timeout = 2)
-  */
-
-  /* login
-  def login(self):
-        # We first have to make a request to index.php to get the csrf token
-        request_url = f"https://{self.school}.edupage.org/login/index.php"
-
-        csrf_response = self.session.get(request_url).content.decode()
-
-        # Parse the token from the HTML
-        csrf_token = csrf_response.split(
-            "name=\"csrfauth\" value=\"")[1].split("\"")[0]
-
-        # Now is everything the same as in the first approach, we just add the csrf token
-        parameters = {
-            "username": self.username,
-            "password": self.password,
-            "csrfauth": csrf_token
-        }
-        request_url = f"https://{self.school}.edupage.org/login/edubarLogin.php"
-
-        response = self.session.post(request_url, parameters)
-
-        if "bad=1" in response.url:
-            raise BadCredentialsException()
-
-        try:
-            self.parse_login_data(response.content.decode())
-        except (IndexError, TypeError):
-            raise LoginDataParsingException()
-
-        return True
-  */
 }
