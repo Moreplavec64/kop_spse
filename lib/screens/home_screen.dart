5eaa27e17be90df05764f10ac433a52cb2313c8b
Moreplavec64 <adamkohadar77@gmail.com>
2021-09-26 10:45:25 +0200

    (+) main screen drawer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,57 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static String route = "/homeScreen";
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  late Animation<double> _animation;
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_animationController);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
     return Scaffold(
-      appBar: AppBar(),
+      key: _scaffoldKey,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            // ak sa uz animuje a pride k stlaceni, zisti ci kade sa ma vratit
+            if (_animationController.isAnimating) {
+              if (_animationController.value < 0.5)
+                _animationController.reverse();
+              else {
+                _animationController.forward();
+              }
+            } else {
+              _animationController.isCompleted
+                  ? _animationController.reverse()
+                  : _animationController.forward();
+            }
+            _scaffoldKey.currentState!.openDrawer();
+          },
+          icon: AnimatedIcon(
+            progress: _animation,
+            icon: AnimatedIcons.close_menu,
+          ),
+        ),
+      ),
+      drawer: Drawer(),
       body: Column(
         children: [
           Center(
@@ -22,5 +66,11 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 }
