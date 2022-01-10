47e2b744775eec45d93d125a99d10a28705c96e0
Moreplavec64 <adamkohadar77@gmail.com>
2022-01-10 21:08:41 +0100

    (+) mappointy suradnice, dijsktra algorithm

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:kop_spse/utils/map_constants.dart';
 import 'package:kop_spse/widgets/appbar.dart';
 import 'package:kop_spse/widgets/home_screen_widgets/drawer.dart';
+import 'package:dijkstra/dijkstra.dart';
+import 'dart:developer' as dev;
 
 class MapScreen extends StatelessWidget {
   static String route = "/homeScreen";
@@ -17,18 +20,30 @@ class MapScreen extends StatelessWidget {
             kBottomNavigationBarHeight);
 
     return Scaffold(
-      key: _scaffoldKey,
-      appBar: CustomAppBar(
-        scaffoldKey: _scaffoldKey,
-        size: MediaQuery.of(context).size,
-      ),
-      drawer: const CustomDrawer(),
-      body: SvgPicture.asset(
-        'assets/images/HB00.svg',
-        height: 600,
-        fit: BoxFit.cover,
-        semanticsLabel: 'mapa skoly',
-      ),
-    );
+        key: _scaffoldKey,
+        appBar: CustomAppBar(
+          scaffoldKey: _scaffoldKey,
+          size: MediaQuery.of(context).size,
+        ),
+        drawer: const CustomDrawer(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SvgPicture.asset(
+                'assets/images/HB00.svg',
+                height: 600,
+                fit: BoxFit.cover,
+                semanticsLabel: 'mapa skoly',
+              ),
+              TextButton(
+                  onPressed: () {
+                    print(Dijkstra.findPathFromGraph(
+                        edges, 'D013-14/Riaditel', 'C108/C109'));
+                    dev.log([...edges.keys].toString());
+                  },
+                  child: Text('Test ALG')),
+            ],
+          ),
+        ));
   }
 }
